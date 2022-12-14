718d6158d4496c35333a7547219776ebf1e55a8a
mehmet uygur <mehmet.uygur@metu.edu.tr>
2022-12-14 11:05:26 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <ExpenseItem></ExpenseItem>
+      |<h2>How you do anything is how you do anything!</h2>
+      <p
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client"; //import reactDOM object in package.json
+
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getEle
