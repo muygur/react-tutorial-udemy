dada2c0434746b08a58d4e62bc444d746385b688
mehmet uygur <mehmet.uygur@metu.edu.tr>
2022-12-16 00:59:20 +0300

    daily commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date();
+  const expenseTitle = `Nutrition`;
+  const expenseAmount = 560.56;
+
   return (
-    <div className = "expense-item">
-      <div>14.12.22</div>
-      <div className = "expense-item__description">
-        <h2>Taso + shoe + gss</h2>
-        <div className = "expense-item__pricegit c">140 + 250 + 700 </div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
     </div>
   );
